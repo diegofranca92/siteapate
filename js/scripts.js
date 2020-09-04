55df37bc8c3c2d5ca83682851b9880d1e7b8358f
Diego França <diego.franca92@gmail.com>
2020-09-04 11:55:39 -0300

    carrosel e ajustes de cores e links

--- js/scripts.js
+++ js/scripts.js
@@ -70,4 +70,26 @@
     }
   });
 
+  $('#recipeCarousel').carousel({
+    interval: 5000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+
 })(jQuery); // End of use strict
